d666d2f1fa7661e020f481c294e5ad6d9f5eb949
grassella <negrete.graciela@yahoo.com>
2025-06-19 14:46:22 -0500

    changes made to docker file

--- backend/app/models/room.py
+++ backend/app/models/room.py
@@ -1,7 +1,6 @@
-from .db import db
-
 class Room(db.Model):
     __tablename__ = 'rooms'
+    __table_args__ = {'schema': 'graciela_cap'}  # add this line
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -14,4 +13,3 @@ class Room(db.Model):
             "name": self.name,
             "plants": [plant.to_dict() for plant in self.plants]
         }
- 
