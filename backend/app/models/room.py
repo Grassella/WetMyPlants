cf89226d6a17209e18c820cebdb72d23a1dac326
grassella <negrete.graciela@yahoo.com>
2025-06-19 15:12:50 -0500

    changed room.py again

--- backend/app/models/room.py
+++ backend/app/models/room.py
@@ -1,7 +1,12 @@
+import os
 from .db import db
+
+SCHEMA = os.environ.get("SCHEMA", "public")
+schema_args = {'schema': SCHEMA} if SCHEMA != 'public' else {}
 
 class Room(db.Model):
     __tablename__ = 'rooms'
+    __table_args__ = schema_args
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -14,4 +19,3 @@ class Room(db.Model):
             "name": self.name,
             "plants": [plant.to_dict() for plant in self.plants]
         }
- 
