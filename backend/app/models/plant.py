d37e8f78001f83d417e5f96ccdb19abf51abb28f
grassella <negrete.graciela@yahoo.com>
2025-06-20 19:17:15 -0500

    added fks to plant.py

--- backend/app/models/plant.py
+++ backend/app/models/plant.py
@@ -1,13 +1,16 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
 class Plant(db.Model):
     __tablename__ = 'plant'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True) 
-    room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('rooms.id'), nullable=False))
     api_id = db.Column(db.Integer)
     common_name = db.Column(db.String)
     image_url = db.Column(db.String)
